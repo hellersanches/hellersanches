a70fa81e34e654184fe7726807718467b7e27680
Heller Sanches <hellersanches@gmail.com>
2025-04-19 18:46:09 +0100

    update DB

--- hostel/settings.py
+++ hostel/settings.py
@@ -80,13 +80,13 @@ WSGI_APPLICATION = 'hostel.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-
-    }
-
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',  # usa SQLite localmente
+        conn_max_age=600,                # mantém a conexão aberta por mais tempo
+        ssl_require=True                 # importante para conexão segura com Render
+    )
 }
+
 
 
 # Password validation
