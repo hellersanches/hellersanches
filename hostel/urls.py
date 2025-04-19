9a28b7e10994b7fb270391fc0c546c6aa904eb2d
Heller Sanches <hellersanches@gmail.com>
2025-04-19 19:27:05 +0100

    Rotas adicionadas

--- hostel/urls.py
+++ hostel/urls.py
@@ -1,7 +1,9 @@
+from django.shortcuts import redirect
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', lambda request: redirect('/api/')),
     path('admin/', admin.site.urls),
     path('api/', include('core.urls')),
 ]
