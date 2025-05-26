fa2754eadff179190cd8443bee0466a968cf115d
Heller Sanchez <hellersanches@gmail.com>
2025-05-26 17:50:49 +0100

    Corrige ALLOWED_HOSTS para produção no Render

--- cadastro_clientes/settings.py
+++ cadastro_clientes/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +27,7 @@ SECRET_KEY = 'django-insecure-h%y&8rz-87ojq@^nev2-1bg*nfx&(x8p+oy_q2)x_f901d_jq=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['cadastro-2-eoo1.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -130,7 +132,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
